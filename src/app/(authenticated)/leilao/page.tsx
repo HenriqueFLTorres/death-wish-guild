9f1a1190a90b86c75760ce130afa6b57ffefeecf
norv1 <kelven.fellipe1@outlook.com>
2024-11-06 23:40:28 -0300

    feat: changed auction page

--- src/app/(authenticated)/leilao/page.tsx
+++ src/app/(authenticated)/leilao/page.tsx
@@ -7,16 +7,15 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table"
+import { AuctionsHeader } from "./_components/AuctionsHeader"
 import { columns } from "./_components/columns"
 import { DataTable } from "./_components/data-table"
-import { NewAuction } from "./_components/NewAuction"
 import { RouterOutput, trpc } from "@/trpc-client/client"
 
 export type AuctionType = RouterOutput["auctions"]["getAuctions"][number]
 
 export default function Inventory() {
   const { data: auctions = [] } = trpc.auctions.getAuctions.useQuery()
-
   const table = useReactTable<AuctionType>({
     data: auctions,
     columns,
@@ -30,8 +29,8 @@ export default function Inventory() {
   return (
     <div className="flex h-full w-full items-center justify-center py-20">
       <section className="relative flex h-full w-full max-w-screen-2xl flex-col gap-4 overflow-auto rounded-lg border border-neutral-800 bg-gradient-to-b from-neutral-900 to-neutral-900/50 text-neutral-100">
+        <AuctionsHeader table={table} />
         <DataTable columns={columns} table={table} />
-        <NewAuction />
       </section>
     </div>
   )
